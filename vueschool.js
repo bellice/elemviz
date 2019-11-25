5334b391852f2dc52344b417efc2c6f36b45f005
bellice <bertrand.miroux@cget.gouv.fr>
2019-11-25 18:08:35 +0100

    vueschool fundamentals

--- vueschool.js
+++ vueschool.js
@@ -1,27 +1,137 @@
-// Vue.js Fundamental
+"use strick";
 
-
+// #1 Vue.js Fundamental
 let shoppingList1 = new Vue({
     el: "#shopping-list1",
     data: {
-        header : "Vue est fantastique!"
+        msg: "Vue est fantastique !"
     }
 })
 
-//  Vue.js Template Syntax And Expressions 
+shoppingList1.$data.msg = "Directement depuis la console"
 
+
+// #2 Vue.js Template Syntax And Expressions 
 let shoppingList2 = new Vue({
-    el : "#shopping-list2",
-    data : {
-        state : "default",
-        header : "Liste de courses".toLocaleUpperCase(),
-        newItem : "",
-        items : [
-            // "2 paquets de pâtes",
-            // "3 sauces tomates",
-            // "1 bouquet de basilic"
-    ]
-    }, 
+    el: "#shopping-list2",
+    data: {
+        msg: "Vue est prêt"
+    }
+})
+
+
+// #3 List Rendering
+let shoppingList3 = new Vue({
+    el: "#shopping-list3",
+    data: {
+        msg: "Liste des programmes",
+        items: [
+            "France Services",
+            "Territoire d'industrie",
+            "Action coeur de ville",
+            "France numérique",
+            "Petites centralités"
+        ]
+    }
+})
+
+shoppingList3.$data.items.push("Contrats de ruralité");
+shoppingList3.$data.items.pop();
+
+
+// #4 User Inputs & Vue Devtools
+let shoppingList4 = new Vue({
+    el: "#shopping-list4",
+    data: {
+        msg: "Liste des programmes",
+        newItem: "",
+        items: [
+            "France Services",
+            "Territoire d'industrie",
+            "Action coeur de ville",
+            "France numérique",
+            "Petites centralités"
+        ]
+    }
+})
+
+shoppingList4.$data.newItem = "Contrat de transition écologique"
+
+
+// #5 User Events
+let shoppingList5 = new Vue({
+    el: "#shopping-list5",
+    data: {
+        msg: "Liste des programmes",
+        newItem: "",
+        items: [
+            "France Services",
+            "Territoire d'industrie",
+            "Action coeur de ville",
+            "France numérique",
+            "Petites centralités"
+        ]
+    }
+})
+
+console.log(shoppingList5.$data.items)
+shoppingList5.$data.newItem = "Nouveau programme"
+shoppingList5.$data.items.push(shoppingList5.$data.newItem)
+
+/*
+Sur la console avec Vue.js dev.tools
+
+$vm0.items
+$vm0.newItem= "Nouveau programme par le dev"
+$vm0.items.push("$vm0.newItem")
+*/
+
+
+// #6 Vue Methods
+let shoppingList6 = new Vue({
+    el: "#shopping-list6",
+    data: {
+        msg: "Liste des programmes",
+        newItem: "",
+        items: [
+            "France Services",
+            "Territoire d'industrie",
+            "Action coeur de ville",
+            "France numérique",
+            "Petites centralités"
+        ]
+    },
+    methods: {
+        saveItem: function(){
+            this.items.push(this.newItem);
+            this.newItem = "";
+        }
+    }
+})
+
+/*
+Sur la console avec Vue.js dev.tools
+
+$vm0.saveItem
+*/
+
+
+
+// #7 Conditional Rendering
+let shoppingList7 = new Vue({
+    el: "#shopping-list7",
+    data: {
+        state: "default",
+        msg: "Liste des programmes",
+        newItem: "",
+        items: [
+            //"France Services",
+            //"Territoire d'industrie",
+            //"Action coeur de ville",
+            //"France numérique",
+            //"Petites centralités"
+        ]
+    },
     methods: {
         saveItem: function(){
             this.items.push(this.newItem);
@@ -32,23 +142,86 @@ let shoppingList2 = new Vue({
             this.newItem = "";
         }
     }
-});
+})
 
 
 
+// #8 Attribute Bindings
+let shoppingList8 = new Vue({
+    el: "#shopping-list8",
+    data: {
+        state: "default",
+        msg: "Liste des programmes",
+        newItem: "",
+        items: [
+            "France Services",
+            "Territoire d'industrie",
+            "Action coeur de ville",
+            "France numérique",
+            "Petites centralités"
+        ]
+    },
+    methods: {
+        saveItem: function(){
+            this.items.push(this.newItem);
+            this.newItem = "";
+        },
+        changeState: function(newState){
+            this.state = newState;
+            this.newItem = "";
+        }
+    }
+})
 
-
-
-
-
-
-
-
-
-
-
-
-
-// {{}}
-// v-for
-// v-on
+// #9 Dynamic Classes
+let shoppingList9 = new Vue({
+    el: "#shopping-list9",
+    data: {
+        state: "default",
+        msg: "Liste des programmes",
+        newItem: "",
+        items: [
+            {
+                label: "France Services",
+                service: false,
+                hightPriority: true,
+            },
+            {
+                label: "Territoire d'industrie",
+                service: true,
+                hightPriority: false,
+            },
+            {
+                label: "Action coeur de ville",
+                service: true,
+                hightPriority: false,
+            },
+            {
+                label: "France numérique",
+                service: false,
+                hightPriority: true,
+            },
+            {
+                label: "Petites centralités",
+                service: false,
+                hightPriority: false,
+            },
+        ]
+    },
+    methods: {
+        saveItem: function(){
+            this.items.push({
+                label: this.newItem,
+                service: false,
+            },);
+            this.newItem = "";
+        },
+        changeState: function(newState){
+            this.state = newState;
+            this.newItem = "";
+        },
+        toggleService: function(item){
+            item.service = !item.service;
+        }
+    }
+})
